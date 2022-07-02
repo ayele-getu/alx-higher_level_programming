094f23266e79881f3bf760d194aa37c97373f846
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-07-02 12:05:52 +0300

    This is the commit for all taks of '0x03. Python - Data Structures: Lists, Tuples' python project

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None or len(my_list) is 0:
+    if not my_list:
         return None
-    big = my_list[0]
-    for i in my_list:
-        if i > big:
-            big = i
-    return(big)
+    my_list.sort()
+    return my_list[-1]
