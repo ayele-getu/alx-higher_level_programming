bc6b422ecacf6d5517cb7cbad275306186fe382b
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-07-06 13:45:54 +0300

    This is the commit for all tasks of python pyoject which is called '0x04. Python - More Data Structures: Set, Dictionary'

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
-        new_row = list([x**2 for x in row])
-        new_matrix.append(new_row)
-    return(new_matrix)
-
-'''
-return[[n**2 for n in row] for row in matrix]
-'''
+    return [[i**2 for i in j] for j in matrix]
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    new_list = [x if x != search else replace for x in my_list]
-    return(new_list)
-
-'''
-    new_list = []
-    for x in my_list:
-        if val == search:
-            new_list.append(replace)
-        else:
-            new_list.append(x)
-    return new_list
-'''
+    return [replace if i == search else i for i in my_list]
