3be92f5829ae43e39f5ea92260774aabfa795764
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-07-19 14:00:10 +0300

    The commit for all tasks of '0x06. Python - Classes and Objects' python project

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-class Square():
-    """A square class."""
-    def __init__(self):
-        """Initialization of the class square."""
-        pass
+"""Example Empty Class Square"""
+
+
+class Square:
+    # Pass is necesary, because i need a one line into class
+    pass
