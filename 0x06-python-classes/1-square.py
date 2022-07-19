c890746f5ca6dc2a32e83949473c76b8580f20a3
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-07-19 14:20:20 +0300

    The commit for all tasks of '0x06. Python - Classes and Objects' python project

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-class Square():
-    """A square class."""
+class Square:
     def __init__(self, size):
-        """Initialization of the class square."""
         self.__size = size
