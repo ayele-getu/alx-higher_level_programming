20b0a748c7945078860a8e24e50e994f95b91e2c
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-08-02 15:51:12 +0300

    The commit for the task_02 in python project called '0x0B. Python - Input/Output'

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,8 +2,8 @@
 """ A function ppending a text to a file"""
 
 
-def append_write(filename="", text="") as file_oblject:
+def append_write(filename="", text=""):
     """Appending a text to UTF8 file"""
 
-    with open(filename, mode="a", encoding="utf-8"):
+    with open(filename, mode="a", encoding="utf-8") as file_object:
         file_object.append(text)
