6c07067d480642c3883a05d6fc4e44dda64562ae
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-08-02 15:26:36 +0300

    The commit for the task_01 in python project called '0x0B. Python - Input/Output'

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """writing a text to a file"""
 
+
 def write_file(filename = "", text = ""):
     """write a text file to utf8"""
+
     with open(filename, mode = "w", encoding = "utf-8") as file_object:
-        file_object.write("UTF8")
-        print(len(fileobject))
+        file_object.write("text")
+        print(len(file_object))
